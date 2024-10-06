185b802ae60151f97d29ae6181c9f946356601a4
MarcusLages <marcusvlages@gmail.com>
2024-10-05 17:26:01 -0700

    Added initializer_list to vector1

--- abstraction/destructor/destructor.cpp
+++ abstraction/destructor/destructor.cpp
@@ -1,11 +1,22 @@
+#include <initializer_list>
+#include <algorithm>    // For std::copy
+
 class Vector1 {
 private:
     double *elem;
     int sz;
 public:
-    Vector1(int s) {
-        elem = new double[s];
-        sz = s;
+    // C++-like constructor
+    Vector1(int s) :
+        elem {new double[s]},
+        sz {s}
+    {}
+
+    Vector1(std::initializer_list<double> init_list) :
+        elem {new double[init_list.size()]},
+        sz {init_list.size()}
+    {
+        std::copy(init_list.begin(),init_list.end(), elem);
     }
     
     double & operator[](int i) {
